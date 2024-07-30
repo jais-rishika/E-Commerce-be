fd747dc12d946f56308f0185c38c0bcea5da409e
Rishika Jaiswal <jaisrishika@gmail.com>
2024-07-30 15:31:57 +0530

    category Contoller insert and delete added

--- Routes/categoryRoutes.js
+++ Routes/categoryRoutes.js
@@ -3,11 +3,13 @@ const router=express()
 // const {verifyIsLoggedIn ,verifyIsAdmin} = require("../middleware/verifyAuthToken");
 
 
-const {getCategories, saveAttr}=require("../controllers/categoryController")
+const {getCategories, saveAttr, newCategory,deleteCategory}=require("../controllers/categoryController")
 
 // router.use(verifyIsLoggedIn)
 // router.use(verifyIsAdmin)
 router.get("/",getCategories)
+router.post("/",newCategory)
+router.delete("/:category",deleteCategory)
 router.post("/attribute",saveAttr)
 
 module.exports=router
